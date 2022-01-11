097eaaa36666bde52bbe891022677199bbc5cc2a
Diego Alves <dalveshomeoffice@gmail.com>
2022-01-11 17:24:58 -0300

    up

--- src/components/routes/stack.routes.tsx
+++ src/components/routes/stack.routes.tsx
@@ -1,48 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from "@react-navigation/stack";
 
-import { Home } from '../../screens/Home';
-import { CarDetails } from '../../screens/CarDetails';
-import { Scheduling } from '../../screens/Scheduling';
-import { SchedulingDetails } from '../../screens/SchedulingDetails';
-import { SchedulingCompleted } from '../../screens/SchedulingCompleted';
-import { MyCars } from '../../screens/MyCars';
+import { Home } from "../../screens/Home";
+import { CarDetails } from "../../screens/CarDetails";
+import { Scheduling } from "../../screens/Scheduling";
+import { SchedulingDetails } from "../../screens/SchedulingDetails";
+import { SchedulingCompleted } from "../../screens/SchedulingCompleted";
+import { MyCars } from "../../screens/MyCars";
+import { Splash } from "../../screens/Splash";
 
 const { Navigator, Screen } = createStackNavigator();
 
-export function StackRoutes(){
-    return(
-        <Navigator headerMode="none">
-            <Screen 
-                name="Home"
-                component={Home}     
-            />
+export function StackRoutes() {
+  return (
+    <Navigator headerMode="none" initialRouteName="Splash">
+      <Screen name="Splash" component={Splash} />
 
-            <Screen 
-                name="CarDetails"
-                component={CarDetails}     
-            />
+      <Screen name="Home" component={Home} />
 
-            <Screen 
-                name="Scheduling"
-                component={Scheduling}     
-            />
+      <Screen name="CarDetails" component={CarDetails} />
 
-            <Screen 
-                name="SchedulingDetails"
-                component={SchedulingDetails}     
-            />
-            
-            <Screen 
-                name="SchedulingCompleted"
-                component={SchedulingCompleted}     
-            />
-        
-            <Screen 
-                name="MyCars"
-                component={MyCars}     
-            />
-        </Navigator>
-    )
+      <Screen name="Scheduling" component={Scheduling} />
+
+      <Screen name="SchedulingDetails" component={SchedulingDetails} />
+
+      <Screen name="SchedulingCompleted" component={SchedulingCompleted} />
+
+      <Screen name="MyCars" component={MyCars} />
+    </Navigator>
+  );
 }
---
+++ src/screens/Splash/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button } from "react-native";
+import Animated from "react-native-reanimated";
+import { Container } from "./styles";
+
+export function Splash() {
+  return (
+    <Container>
+      <Animated.View></Animated.View>
+
+      <Button title="Mover" onPress={() => {}} />
+    </Container>
+  );
+}
+
+Splash Screen
+11:08
